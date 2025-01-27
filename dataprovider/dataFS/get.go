5c208cf664ca9df664e2c1f2e789e86ec74d1e7b
Григорий Никониров <mrgbh007@gmail.com>
2025-01-27 13:00:44 +0700

    Исправление некорректной обработки ошибки при отсутствии файла

--- dataprovider/dataFS/get.go
+++ dataprovider/dataFS/get.go
@@ -8,12 +8,19 @@ import (
 	"os"
 
 	"github.com/google/uuid"
+
+	"github.com/gbh007/hgraber-next-agent-core/entities"
 )
 
 func (s *Storage) Get(ctx context.Context, fileID uuid.UUID) (io.Reader, error) {
 	filepath := s.filepath(fileID)
 
 	f, err := os.Open(filepath)
+
+	if os.IsNotExist(err) {
+		return nil, entities.FileNotFoundError
+	}
+
 	if err != nil {
 		return nil, fmt.Errorf("local fs: open: %w", err)
 	}
